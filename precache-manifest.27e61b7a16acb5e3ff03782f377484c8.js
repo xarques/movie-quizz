b6e1807e68773953cdc92da9d311da16bb4eed94
Xavier Arques <xavarques@gmail.com>
2019-01-17 02:24:56 +0100

    Updates

---
+++ precache-manifest.27e61b7a16acb5e3ff03782f377484c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "472a14e4b55f1089e3b1",
+    "url": "/movie-quiz/static/js/main.472a14e4.chunk.js"
+  },
+  {
+    "revision": "9ea72ed1ebf6ab3599ea",
+    "url": "/movie-quiz/static/css/1.3e68fc91.chunk.css"
+  },
+  {
+    "revision": "9ea72ed1ebf6ab3599ea",
+    "url": "/movie-quiz/static/js/1.9ea72ed1.chunk.js"
+  },
+  {
+    "revision": "ba3431457d2cf4bd7948",
+    "url": "/movie-quiz/static/js/runtime~main.ba343145.js"
+  },
+  {
+    "revision": "6719c3c1703a9e1bcab1344323ad536b",
+    "url": "/movie-quiz/index.html"
+  }
+];
